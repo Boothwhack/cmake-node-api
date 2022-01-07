7344ad73cfde5fc4993f8417a9f3fda7de8d18d1
Karoline <boothwhack@users.noreply.github.com>
2022-01-07 15:24:49 +0100

    Fix script path

--- cmake/ResolveNodeVersion.cmake
+++ cmake/ResolveNodeVersion.cmake
@@ -1,6 +1,6 @@
 function(ResolveNodeVersion requestedVersion out)
     execute_process(
-            COMMAND node "${CMAKE_CURRENT_LIST_DIR}/../identify-version.js" "${requestedVersion}"
+            COMMAND node "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../identify-version.js" "${requestedVersion}"
             OUTPUT_VARIABLE version
             OUTPUT_STRIP_TRAILING_WHITESPACE
             COMMAND_ERROR_IS_FATAL ANY
