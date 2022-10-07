3ad60c5265a89c1491b3a87301a3854b01606e74
Karoline <boothwhack@users.noreply.github.com>
2022-10-07 09:47:41 +0200

    Improve find modules

--- cmake/FindNodeAddonApi.cmake
+++ cmake/FindNodeAddonApi.cmake
@@ -3,7 +3,16 @@ include(ExecuteNode)
 execute_node("require('node-addon-api').include"
         TRIM_QUOTES
         OUTPUT_VARIABLE naa_include_dir
+        ERROR_VARIABLE naa_error
+        RESULT_VARIABLE naa_result
         WORKING_DIRECTORY ${NODE_PACKAGE_DIR})
 
-add_library(NodeAddonApi INTERFACE)
-target_include_directories(NodeAddonApi INTERFACE ${naa_include_dir})
+if (naa_result)
+    if (NodeAddonApi_FIND_REQUIRED)
+        message(FATAL_ERROR "Could not find NodeAddonApi:\n${naa_error}\nEnsure that the 'node-addon-api' package is installed properly.")
+    endif ()
+else ()
+    add_library(_NodeAddonApi INTERFACE)
+    target_include_directories(_NodeAddonApi INTERFACE ${naa_include_dir})
+    add_library(NodeAddonApi::NodeAddonApi ALIAS _NodeAddonApi)
+endif ()
--- cmake/FindNodeApi.cmake
+++ cmake/FindNodeApi.cmake
@@ -1,35 +1,37 @@
 include(ResolveNodeVersion)
 ResolveNodeVersion("${NodeApi_FIND_VERSION}" version)
 
-set(node_content_dir "${CMAKE_CURRENT_BINARY_DIR}/node-${version}")
-set(node_headers_tarball "${node_content_dir}/node-${version}-headers.tar.gz")
-set(node_headers_dir "${node_content_dir}/node-headers")
+set(node_content_dir "${CMAKE_BINARY_DIR}/node-${version}")
 
-# Setup Node Api headers
-file(DOWNLOAD "https://nodejs.org/dist/${version}/node-${version}-headers.tar.gz" "${node_headers_tarball}")
-file(ARCHIVE_EXTRACT INPUT "${node_headers_tarball}" DESTINATION "${node_headers_dir}")
+function(SetupNodeHeaders)
+    set(node_headers_tarball "${node_content_dir}/node-${version}-headers.tar.gz")
+    set(node_headers_dir "${node_content_dir}/node-headers")
 
-set(node_headers_include_dir "${node_headers_dir}/node-${version}/include/node")
+    file(DOWNLOAD "https://nodejs.org/dist/${version}/node-${version}-headers.tar.gz" "${node_headers_tarball}")
+    file(ARCHIVE_EXTRACT INPUT "${node_headers_tarball}" DESTINATION "${node_headers_dir}")
 
-# Windows needs to link to node.lib
-if (WIN32)
-    if (CMAKE_SYSTEM_PROCESSOR STREQUAL "AMD64")
-        set(node_windows_arch "x64")
-    elseif (CMAKE_SYSTEM_PROCESSOR STREQUAL "x86")
-        set(node_windows_arch "x86")
-    else ()
-        message(FATAL_ERROR "Incompatible system processor: ${CMAKE_SYSTEM_PROCESSOR}")
+    set(node_include_dir "${node_headers_dir}/node-${version}/include/node" PARENT_SCOPE)
+endfunction()
+
+if (NOT TARGET NodeApi)
+    # Download headers
+    SetupNodeHeaders()
+
+    # Windows needs to link to node.lib
+    if (WIN32)
+        set(node_lib_file "${node_content_dir}/win-${NODE_ARCH}/node.lib")
+        set(node_lib_url "https://nodejs.org/dist/${version}/win-${NODE_ARCH}/node.lib")
+
+        file(DOWNLOAD "${node_lib_url}" "${node_lib_file}")
+
+        add_library(NodeApi SHARED IMPORTED)
+        set_target_properties(NodeApi PROPERTIES IMPORTED_IMPLIB "${node_lib_file}")
+        target_include_directories(NodeApi INTERFACE "${node_include_dir}")
+
+    elseif (UNIX AND NOT APPLE)
+        add_library(NodeApi INTERFACE)
+        target_include_directories(NodeApi INTERFACE "${node_include_dir}")
     endif ()
 
-    set(node_lib_file "${node_content_dir}/win-${node_windows_arch}/node.lib")
-    set(node_lib_url "https://nodejs.org/dist/${version}/win-${node_windows_arch}/node.lib")
-
-    file(DOWNLOAD "${node_lib_url}" "${node_lib_file}")
-
-    add_library(NodeApi UNKNOWN IMPORTED)
-    set_target_properties(NodeApi PROPERTIES IMPORTED_LOCATION "${node_lib_file}")
-else ()
-    add_library(NodeApi INTERFACE)
+    add_library(Node::Api ALIAS NodeApi)
 endif ()
-
-target_include_directories(NodeApi INTERFACE ${node_headers_include_dir})
